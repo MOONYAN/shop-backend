79395bc4a0cc73a81a4acd49535a5581ab49e089
MOONYAN <c8763sasuke@ntut.org.tw>
2020-09-10 22:41:25 +0800

    impliment user repo

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,13 @@
+import { UserEntity } from './user.entity';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity])
+  ],
   providers: [UserService],
-  exports:[UserService]
+  exports: [UserService]
 })
 export class UserModule { }
